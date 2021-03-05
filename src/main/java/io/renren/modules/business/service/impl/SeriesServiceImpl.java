55bf69f25df82d8c9a5a3f19e1b8d42bc913ddb3
allan <zwy1997213@163.com>
2021-03-05 15:24:52 +0800

    完善汽车查询接口

--- src/main/java/io/renren/modules/business/service/impl/SeriesServiceImpl.java
+++ src/main/java/io/renren/modules/business/service/impl/SeriesServiceImpl.java
@@ -1,5 +1,6 @@
 package io.renren.modules.business.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,30 @@ public class SeriesServiceImpl extends ServiceImpl<SeriesDao, SeriesEntity> impl
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<SeriesEntity> seriesEntityQueryWrapper = new QueryWrapper<>();
+
+        // 解析查询参数
+        Object nameObj = params.get("seriesName");
+        if(nameObj != null){
+            String seriesName = nameObj.toString();
+            if(StringUtils.isNotBlank(seriesName)){
+                seriesEntityQueryWrapper.lambda().like(SeriesEntity::getSeriesName,seriesName);
+            }
+        }
+
+        Object typeObj = params.get("carType");
+        if(typeObj != null){
+            String carType = typeObj.toString();
+            if(StringUtils.isNotBlank(carType)){
+                String[] types = carType.split(",");
+                seriesEntityQueryWrapper.lambda().in(SeriesEntity::getLevelName,types);
+            }
+        }
+
         IPage<SeriesEntity> page = this.page(
                 new Query<SeriesEntity>().getPage(params),
-                new QueryWrapper<SeriesEntity>()
+                seriesEntityQueryWrapper
         );
 
         return new PageUtils(page);
